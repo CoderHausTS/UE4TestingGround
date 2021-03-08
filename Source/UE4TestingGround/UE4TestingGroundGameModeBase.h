aa220427fdd8c4c7a3f37cb40b3f3ea398c69ec5
Jason Woods <jason@coder.haus>
2021-03-08 07:13:18 -0600

    Initial

---
+++ Source/UE4TestingGround/UE4TestingGroundGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4TestingGroundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4TESTINGGROUND_API AUE4TestingGroundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
